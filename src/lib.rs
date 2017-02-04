c778669a927c083b449b63d0a32b2238a3ec184d
Stephen Holsapple <sholsapp@gmail.com>
2017-02-04 08:02:47 -0800

    Define the struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,42 @@
+use std::sync::{Arc, Mutex, Condvar};
+use std::thread;
+
+pub struct Timer<'a> {
+    pub is_alive: bool,
+    pub was_reset: bool,
+    timed_out: &'a Condvar,
+    cv: Condvar,
+    cv_m: Mutex<bool>,
+    step: u64,
+    jitter: u64,
+    thread: Option<std::thread::Thread>,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(timed_out: &'a Condvar) -> Timer {
+        Timer {
+            is_alive: false,
+            was_reset: false,
+            timed_out: timed_out,
+            step: 2,
+            jitter: 0,
+            thread: None,
+            cv_m: Mutex::new(false),
+            cv: Condvar::new(),
+
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::sync::Condvar;
+    use Timer;
+
     #[test]
     fn it_works() {
+        let cv = Condvar::new();
+        let t = Timer::new(&cv);
+        assert!(t.is_alive == false);
     }
 }
