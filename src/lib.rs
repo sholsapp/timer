b31212d62994a53cbd94ec1593c7500c246d3f9b
Stephen Holsapple <sholsapp@gmail.com>
2017-02-05 07:56:42 -0800

    Refactor step from `u64` to `Duration`.

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ pub struct Timer {
     // Condition variable signalled if/when timer expires.
     pub timed_out: Arc<Condvar>,
     // The amount of time in nanoseconds to count down from.
-    pub step: u64,
+    pub step: Duration,
     // True if the timer is counting down.
     pub alive: Arc<AtomicBool>,
     /// Number of times this timer has expired.
@@ -35,7 +35,7 @@ impl Timer {
     ///
     /// * `timed_out` - Condition to signal if the timer expires.
     ///
-    pub fn new(step: u64, timed_out: Arc<Condvar>) -> Timer {
+    pub fn new(step: Duration, timed_out: Arc<Condvar>) -> Timer {
         Timer {
             handle: None,
             alive: Arc::new(AtomicBool::new(false)),
@@ -48,17 +48,19 @@ impl Timer {
     }
     /// Internal timer loop.
     ///
-    fn spin(alive: Arc<AtomicBool>, cv: Arc<Condvar>, m: Arc<Mutex<bool>>, timed_out: Arc<Condvar>, expiries: Arc<AtomicUsize>, step: u64) {
+    fn spin(alive: Arc<AtomicBool>,
+            cv: Arc<Condvar>,
+            m: Arc<Mutex<bool>>,
+            timed_out: Arc<Condvar>,
+            expiries: Arc<AtomicUsize>,
+            step: Duration) {
         alive.store(true, Ordering::SeqCst);
         while alive.load(Ordering::SeqCst) {
-            match cv.wait_timeout(m.lock().unwrap(), Duration::new(step, 0)) {
+            match cv.wait_timeout(m.lock().unwrap(), step) {
                 Ok((_, result)) => {
                     if result.timed_out() {
-                        // println!("Signaling waiters!");
                         expiries.fetch_add(1, Ordering::SeqCst);
                         timed_out.notify_all();
-                    } else {
-                        // println!("Resetting timer!");
                     }
                 },
                 Err(e) => {
@@ -98,24 +100,35 @@ impl Timer {
 #[test]
 fn it_works() {
     let cv = Arc::new(Condvar::new());
-    let t = Timer::new(5, cv);
+    let d = Duration::from_secs(5);
+    let t = Timer::new(d, cv);
     assert!(t.alive.load(Ordering::SeqCst) == false);
 }
 
 #[test]
 fn timer_start() {
     let cv = Arc::new(Condvar::new());
-    let mut t = Timer::new(5, cv);
+    let mut t = Timer::new(Duration::from_millis(50), cv);
     t.start();
-    std::thread::sleep(Duration::new(10, 0));
-    std::thread::sleep(Duration::new(1, 0));
-    t.reset();
-    std::thread::sleep(Duration::new(10, 0));
+    // This should cause at least two expiries...
+    std::thread::sleep(Duration::from_millis(100));
     t.stop();
-    assert!(t.expiries.load(Ordering::SeqCst) == 4);
+    assert!(t.expiries.load(Ordering::SeqCst) >= 2);
+    assert!(t.expiries.load(Ordering::SeqCst) < 5);
 }
 
 #[test]
 fn timer_reset() {
-
+    let cv = Arc::new(Condvar::new());
+    let mut t = Timer::new(Duration::from_millis(50), cv);
+    t.start();
+    // This should cause two expiries...
+    std::thread::sleep(Duration::from_millis(125));
+    // This should catch an expiry before it happens...
+    t.reset();
+    // This should cause two expiries...
+    std::thread::sleep(Duration::from_millis(100));
+    t.stop();
+    assert!(t.expiries.load(Ordering::SeqCst) >= 4);
+    assert!(t.expiries.load(Ordering::SeqCst) < 6);
 }
